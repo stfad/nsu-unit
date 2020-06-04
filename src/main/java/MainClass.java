8f1d40a806e8d39b30ce0ce9bbc3a3b2935ca953
Stepan Fadeev <sfadeev@ac-sw.com>
2020-06-05 06:04:26 +0700

    - Добавлена очередь и набор тестировщиков классов

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
         System.out.println("Hello from NSUUnit!");
         for(int i=0; i < args.length; i++)
             System.out.println("args[" + i + "] : " + args[i]);
+        QueueOfClassesToTest queue = new QueueOfClassesToTest(3);
     }
 }
